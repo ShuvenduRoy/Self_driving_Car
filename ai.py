e0d11564da3e1b2104d93b2c2351b57610efed75
Shuvendu Bikash <bikash11roy@gmail.com>
2017-10-21 16:19:21 +0600

    class for deep Q network added
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- ai.py
+++ ai.py
@@ -46,6 +46,21 @@ class ReplayMemory(object):
         return map(lambda x: Variable(torch.cat(x, 0)), samples)
 
 
+# Implementing Deep Q Learning
+class Dqn():
+
+    def __init__(self, input_size, nb_action, gamma):
+        self.gamma = gamma
+        self.reward_window = []
+        self.model = Network(input_size, nb_action)
+        self.memory = ReplayMemory(100000)
+        self.optimizer = optim.Adam(self.model.parameters, lr=0.001)
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        self.last_action = 0
+        self.last_reward = 0
+        
+    def select_action(self, state):
+        
         
         
         
