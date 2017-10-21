0a7dce90dc6f6980a475332f25560995d5cd0fc9
Shuvendu Bikash <bikash11roy@gmail.com>
2017-10-21 16:51:46 +0600

    neural network learning process
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- ai.py
+++ ai.py
@@ -64,7 +64,17 @@ class Dqn():
          action = probs.multinomial()
          return action.data[0, 0]
         
-    
+    # neural network learning process
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        next_output = self.model(batch_next_state).detach().max(1)[0]
+        target =self.gamma*next_output + batch_reward
+        
+        td_loss = F.smooth_l1_loss(outputs, target)
+        self.optimizer.zero_grad()
+        td_loss.backward(retain_variable=True)
+        self.optimizer.step()
+        
         
         
         
