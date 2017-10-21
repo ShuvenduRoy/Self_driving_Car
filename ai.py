4ccd00933bf46984c2302be063b4df30c021d0f8
Shuvendu Bikash <bikash11roy@gmail.com>
2017-10-21 14:50:00 +0600

    importing libraries
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

---
+++ ai.py
@@ -0,0 +1,14 @@
+# Ai for self driving car
+
+# Importing the libraries
+import numpy as np
+import random
+import os
+import torch      # the boss
+import torch.nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+
