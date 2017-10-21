4427b58d94e65134315dd682fe877fbebeb680b5
Shuvendu Bikash <bikash11roy@gmail.com>
2017-10-21 15:06:11 +0600

    creating Nework class with torch
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- ai.py
+++ ai.py
@@ -5,10 +5,19 @@ import numpy as np
 import random
 import os
 import torch      # the boss
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
 
 
+# Creating the architecture of the neural network
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
